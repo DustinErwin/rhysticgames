9436025cbdc951f67208d633107ac2aa42f418bb
Galahad <derwin499@gmail.com>
2026-02-23 21:13:03 -0500

    folder restructure

--- models/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-module.exports = {
-  Event: require("./event"),
-  ObjectId: mongoose.Types.ObjectId,
-};
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-// const memberRoutes = require("./memberRoutes");
-// const cardRoutes = require("./cardRoutes");
-
-// router.use("/member", memberRoutes);
-// router.use("/card", cardRoutes);
-
-module.exports = router;
